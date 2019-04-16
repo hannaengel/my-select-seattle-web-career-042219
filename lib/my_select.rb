3c0b57fa46af5c7695a6afbadf8ec9e029f0e38b
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-16 20:58:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,10 @@ def my_select(collection)
   i = 0
   newArray = []
   while i < collection.length
-    if block_given? == true
-      if yield(array[i])== true
-        newArray<< array[i]
+    if yield(array[i])
+      newArray<< array[i]
       end
       i = i +1
-      end
- newArray
- end
+    end
+    newArray
+end
